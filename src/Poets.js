873fb2bb36657ebf55aac2aa4dabbfd726675c31
Andrew Bonner <andrewbonner92@gmail.com>
2017-12-18 09:24:39 -0500

    Create Poets component.

---
+++ src/Poets.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Poets extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Poets</h2>
+      </div>
+    )
+  }
+}
+
+export default Poets

946e683d552d7ec8a7f168ef21adf2c82ce43822
Andrew Bonner <andrewbonner92@gmail.com>
2017-12-18 09:26:00 -0500

    Pass poets array down through props.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Poets from './Poets'
+import { poets } from './data/poets'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Poets Corner</h1>
-        <Poets />
+        <Poets poets={poets} />
       </div>
     );
   }

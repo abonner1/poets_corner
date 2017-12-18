312aae121547b73368e8ce2fa4d0bd93ff1e7ebc
Andrew Bonner <andrewbonner92@gmail.com>
2017-12-18 09:43:40 -0500

    Render poet list in Poet component.

--- src/Poet.js
+++ src/Poet.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Poet = ({ poet }) => {
   return (
-    <p>Poet info here</p>
+    <li>
+      <p>{poet.firstName} {poet.lastName}</p>
+      <p>Born: {poet.birthDate} - Died: {poet.deathDate}</p>
+      <p>{poet.bio}</p>
+    </li>
   )
 }
 

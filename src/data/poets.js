70d04693161d2479ce027428b1eadf283fc8024e
Andrew Bonner <andrewbonner92@gmail.com>
2017-12-18 09:23:58 -0500

    Add dummy data for testing.

---
+++ src/data/poets.js
@@ -0,0 +1,9 @@
+export var poets = [
+  {
+    firstName: "Edmund",
+    lastName: "Spenser",
+    birthDate: "1552",
+    deathDate: "January 13, 1599",
+    bio: "English poet best known for The Faerie Queen, an epic poem and fantastical allegory celebrating the Tudor dynasty of Elizabeth I."
+  }
+]

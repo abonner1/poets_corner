99ff828b7ea6f9e0875b3e08fa815117cf7022e0
Andrew Bonner <andrewbonner92@gmail.com>
2017-12-18 09:35:56 -0500

    Add second poet to data.

--- src/data/poets.js
+++ src/data/poets.js
@@ -5,5 +5,12 @@ export var poets = [
     birthDate: "1552",
     deathDate: "January 13, 1599",
     bio: "English poet best known for The Faerie Queen, an epic poem and fantastical allegory celebrating the Tudor dynasty of Elizabeth I."
+  },
+  {
+    firstName: "John",
+    lastName: "Milton",
+    birthDate: "December 9, 1608",
+    deathDate: "November 8, 1674",
+    bio: "English poet, polemicist, man of letters, and civil servant for the Commonwealth of England under Oliver Cromwell. He wrote at a time of religious flux and politcal upheaval, and is best known for his epic poem Paradise Lost (1667), writtin in blank verse."
   }
 ]

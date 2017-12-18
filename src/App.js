c2791c91f5f6dae1f56486babd9258a2ea7c0efc
Andrew Bonner <andrewbonner92@gmail.com>
2017-12-18 09:25:05 -0500

    Render Poets component.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import Poets from './Poets'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Poets Corner</h1>
+        <Poets />
       </div>
     );
   }

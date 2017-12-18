bf3c276ae14e0bb2a59f337a0015bf04e0d3ad1f
Andrew Bonner <andrewbonner92@gmail.com>
2017-12-18 09:33:00 -0500

    Refactor poets list to class method.

--- src/Poets.js
+++ src/Poets.js
@@ -6,20 +6,24 @@ class Poets extends Component {
     this.poets = poets
   }
 
+  listPoets() {
+    return this.poets.map((poet) => {
+      return (
+        <li>
+          <p>{poet.firstName} {poet.lastName}</p>
+          <p>Born: {poet.birthDate} - Died: {poet.deathDate}</p>
+          <p>{poet.bio}</p>
+        </li>
+      )
+    })
+  }
+
   render() {
     return (
       <div>
         <h2>Poets</h2>
         <ul>
-          {this.poets.map((poet) => {
-            return (
-              <li>
-                <p>{poet.firstName} {poet.lastName}</p>
-                <p>Born: {poet.birthDate} - Died: {poet.deathDate}</p>
-                <p>{poet.bio}</p>
-              </li>
-            )
-          })}
+          {this.listPoets()}
         </ul>
       </div>
     )

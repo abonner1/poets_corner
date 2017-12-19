85fff8da7cab5df556598270ed723bc9f5a4a6cd
Andrew Bonner <andrewbonner92@gmail.com>
2017-12-19 09:28:57 -0500

    Add React Router to App.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+import Home from './Home'
 import Poets from './Poets'
 import { poets } from './data/poets'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Poets Corner</h1>
-        <Poets poets={poets} />
-      </div>
+      <Router>
+        <div>
+          <li><Link to="/" >Home</Link></li>
+          <li><Link to="/poets" >Poets</Link></li>
+
+          <hr/>
+
+          <Route exact path="/" component={Home} />
+          <Route path="/poets" render={() => (
+            <Poets poets={poets} />
+          )} />
+        </div>
+      </Router>
     );
   }
 }

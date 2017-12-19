ab2ad05fb407573294de7080290516b6c13e8978
Andrew Bonner <andrewbonner92@gmail.com>
2017-12-19 09:55:05 -0500

    Move Poem information rendering to Poem component.

--- src/Poem.js
+++ src/Poem.js
@@ -3,6 +3,8 @@ import React from 'react'
 const Poem = ({ poem }) => {
   return (
     <li>
+      <p>{poem.title} ({poem.publicationDate}) by {poem.author}</p>
+      <p>{poem.about}</p>
     </li>
   )
 }

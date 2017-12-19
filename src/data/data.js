2063eacdd4128b0253ce18955b315f27c4626d5f
Andrew Bonner <andrewbonner92@gmail.com>
2017-12-19 09:51:42 -0500

    Restructure data.

--- src/data/data.js
+++ src/data/data.js
@@ -1,15 +1,3 @@
-export var poems = [
-  {
-    id: 1,
-    title: "The Faerie Queene",
-    author: "Edmund Spenser",
-    authorId: 1,
-    language: "English",
-    publicationDate: 1596,
-    about: "An epic poem by Edmund Spenser, Books I to III were first published in 1590, and then it was republished in 1596 together with books IV to VI. The Faerie Queene is notable for its form: it is one of the longest poems in the English langauge and the origin of a verse from that came to be known as the Spenserian stanza. On a literal level, the poem follows several knights in an examination of several virtues, though it is primarily an allegorical work, and can be read on severl levels, including praise (or, later, criticism) of Queen Elizabeth I."
-  }
-]
-
 export var poets = [
   {
     id: 1,
@@ -26,5 +14,17 @@ export var poets = [
     birthDate: "December 9, 1608",
     deathDate: "November 8, 1674",
     bio: "English poet, polemicist, man of letters, and civil servant for the Commonwealth of England under Oliver Cromwell. He wrote at a time of religious flux and politcal upheaval, and is best known for his epic poem Paradise Lost (1667), writtin in blank verse."
+  }
+]
+
+export var poems = [
+  {
+    id: 1,
+    title: "The Faerie Queene",
+    author: "Edmund Spenser",
+    authorId: 1,
+    language: "English",
+    publicationDate: 1596,
+    about: "An epic poem by Edmund Spenser, Books I to III were first published in 1590, and then it was republished in 1596 together with books IV to VI. The Faerie Queene is notable for its form: it is one of the longest poems in the English langauge and the origin of a verse from that came to be known as the Spenserian stanza. On a literal level, the poem follows several knights in an examination of several virtues, though it is primarily an allegorical work, and can be read on severl levels, including praise (or, later, criticism) of Queen Elizabeth I."
   }
 ]

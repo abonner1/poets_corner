14c70a573f2e3e1dc1cc113775647a29fd68ede8
Andrew Bonner <andrewbonner92@gmail.com>
2017-12-19 09:42:03 -0500

    Create dummy poems data.

---
+++ src/data/poems.js
@@ -0,0 +1,11 @@
+export var poems = [
+  {
+    id: 1,
+    title: "The Faerie Queene",
+    author: "Edmund Spenser",
+    authorId: 1,
+    language: "English",
+    publicationDate: 1596,
+    about: "An epic poem by Edmund Spenser, Books I to III were first published in 1590, and then it was republished in 1596 together with books IV to VI. The Faerie Queene is notable for its form: it is one of the longest poems in the English langauge and the origin of a verse from that came to be known as the Spenserian stanza. On a literal level, the poem follows several knights in an examination of several virtues, though it is primarily an allegorical work, and can be read on severl levels, including praise (or, later, criticism) of Queen Elizabeth I."
+  }
+]

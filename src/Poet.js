d83dbff76a3dd7cec965bda66fa1dedb6df9bfd4
Andrew Bonner <andrewbonner92@gmail.com>
2017-12-20 08:44:51 -0500

    Add test Link to poet.

--- src/Poet.js
+++ src/Poet.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Poet = ({ poet }) => {
+const Poet = ({ poet, url }) => {
   return (
     <li>
-      <p>{poet.firstName} {poet.lastName}</p>
+      <p><Link to={`${url}/edmund_spenser`}>{poet.firstName} {poet.lastName}</Link></p>
       <p>Born: {poet.birthDate} - Died: {poet.deathDate}</p>
       <p>{poet.bio}</p>
     </li>

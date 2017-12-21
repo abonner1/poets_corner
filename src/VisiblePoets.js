879844cc3bab2b70960ab01034bfb8d2867b9e5d
Andrew Bonner <andrewbonner92@gmail.com>
2017-12-21 09:26:48 -0500

    Stub out VisiblePoets container.

---
+++ src/VisiblePoets.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import Poets from './poets'
+
+class VisiblePoets extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <Poets />
+    )
+  }
+}
+
+export default VisiblePoets

64ae5b104169c7151df33e4a656d1578aa19146a
Andrew Bonner <andrewbonner92@gmail.com>
2017-12-21 13:37:33 -0500

    Render poets list and routes in Poets component.

--- src/Poets.js
+++ src/Poets.js
@@ -1,48 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route, Link } from 'react-router-dom'
 
 import { createPath } from './helpers'
 
 import Poet from './Poet'
 
-class Poets extends Component {
-  constructor({ poets, match }) {
-    super()
-    this.poets = poets
-    this.match = match
-  }
-
-  listPoetLinks(poets, url) {
-    return poets.map((poet) => {
-      let path = createPath(poet, url)
-      return (
-        <li key={poet.id}>
-          <p><Link to={path}>{poet.firstName} {poet.lastName}</Link></p>
-        </li>
-      )
-    })
-  }
-
-  listPoetRoutes(poets, url) {
-    return poets.map((poet) => {
-      let path = createPath(poet, url)
-      return (
-        <Route key={poet.id} path={path} render={() => <Poet poet={poet} />} />
-      )
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Poets</h1>
-        <ul>
-          {this.listPoetLinks(this.poets, this.match.url)}
-        </ul>
-        {this.listPoetRoutes(this.poets, this.match.url)}
-      </div>
-    )
-  }
+const Poets = ({ poets, match }) =>  {
+  return (
+    <div>
+      <h1>Poets</h1>
+      <ul>
+        {poets.map((poet) => {
+          let path = createPath(poet, match.url)
+          return (
+            <li key={poet.id}>
+              <p><Link to={path}>{poet.firstName} {poet.lastName}</Link></p>
+            </li>
+          )
+        })}
+      </ul>
+      {poets.map((poet) => {
+        let path = createPath(poet, match.url)
+        return (
+          <Route key={poet.id} path={path} render={() => <Poet poet={poet} />} />
+        )
+      })}
+    </div>
+  )
 }
 
 export default Poets

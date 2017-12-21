b09c5c3b0ca659f20dd3c4416e1df1005cc5385c
Andrew Bonner <andrewbonner92@gmail.com>
2017-12-21 09:24:02 -0500

    Move data file.

---
+++ src/data.js
@@ -0,0 +1,39 @@
+export var poets = [
+  {
+    id: 1,
+    firstName: "Edmund",
+    lastName: "Spenser",
+    birthDate: "1552",
+    deathDate: "January 13, 1599",
+    bio: "English poet best known for The Faerie Queen, an epic poem and fantastical allegory celebrating the Tudor dynasty of Elizabeth I."
+  },
+  {
+    id: 2,
+    firstName: "John",
+    lastName: "Milton",
+    birthDate: "December 9, 1608",
+    deathDate: "November 8, 1674",
+    bio: "English poet, polemicist, man of letters, and civil servant for the Commonwealth of England under Oliver Cromwell. He wrote at a time of religious flux and politcal upheaval, and is best known for his epic poem Paradise Lost (1667), writtin in blank verse."
+  }
+]
+
+export var poems = [
+  {
+    id: 1,
+    title: "The Faerie Queene",
+    author: "Edmund Spenser",
+    authorId: 1,
+    language: "English",
+    publicationDate: 1596,
+    about: "An epic poem by Edmund Spenser, Books I to III were first published in 1590, and then it was republished in 1596 together with books IV to VI. The Faerie Queene is notable for its form: it is one of the longest poems in the English langauge and the origin of a verse from that came to be known as the Spenserian stanza. On a literal level, the poem follows several knights in an examination of several virtues, though it is primarily an allegorical work, and can be read on severl levels, including praise (or, later, criticism) of Queen Elizabeth I."
+  },
+  {
+    id: 2,
+    title: "Paradise Lost",
+    author: "John Milton",
+    authorId: 2,
+    language: "English",
+    publicationDate: 1667,
+    about: "An epic poem by John Milton, the first version was published in 1667, consisting of ten books with over ten thousand lines of verse. The poem concerns the biblical story of the Fall of Man: the temptation of Adam and Eve by the fallen angel Satan and their explusion from the Garden of Eden. Milton's purpose, stated in Book I, is to \"justify the ways of God to me\"."
+  }
+]

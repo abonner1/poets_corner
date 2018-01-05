ac99938a20fffbc14f81b0bfdcab7f7f4cbee321
Andrew Bonner <andrewbonner92@gmail.com>
2018-01-05 08:33:25 -0500

    Move Home to containers and refactor.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
-import Home from './Home'
+import Home from './containers/Home'
 import VisiblePoets from './containers/VisiblePoets'
 import Poems from './containers/Poems'
 import { poems } from './test/data'

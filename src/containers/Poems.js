9354648d51dfa37f02d7fe1da1a17bfa1edc5033
Andrew Bonner <andrewbonner92@gmail.com>
2018-01-08 08:59:19 -0500

    Render links with broken routes.

--- src/containers/Poems.js
+++ src/containers/Poems.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
 
 import Poem from "../components/Poem"
 
 class Poems extends Component {
-  constructor({ poems }) {
+  constructor({ poems, match }) {
     super()
     this.poems = poems
+    this.match = match
   }
 
   listPoems(poems) {
@@ -17,8 +19,15 @@ class Poems extends Component {
       <div>
         <h1>Poems</h1>
         <ul>
-          {this.listPoems(this.poems)}
+          {this.poems.map((poem) => <li key={poem.id}><Link to={poem.title.toLowerCase().split(" ").join("_")}>{poem.title}</Link></li>)}
         </ul>
+
+        {this.poems.map((poem) => {
+          let path = this.match.url + poem.title.toLowerCase().split(" ").join("_")
+          return (
+            <Route key={poem.id} path={path} render={() => <Poem poem={poem} />} />
+          )
+        })}
       </div>
     )
   }

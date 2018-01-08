fccc291f49c8d7f24b1afc71c36283f2d33c12ac
Andrew Bonner <andrewbonner92@gmail.com>
2018-01-08 09:02:58 -0500

    Make Poems stateless.

--- src/containers/Poems.js
+++ src/containers/Poems.js
@@ -1,36 +1,24 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Route, Link } from 'react-router-dom'
 
 import Poem from "../components/Poem"
 
-class Poems extends Component {
-  constructor({ poems, match }) {
-    super()
-    this.poems = poems
-    this.match = match
-  }
+const Poems = ({poems, match}) => {
+  return (
+    <div>
+      <h1>Poems</h1>
+      <ul>
+        {poems.map((poem) => <li key={poem.id}><Link to={match.url + '/' + poem.title.toLowerCase().split(" ").join("_")}>{poem.title}</Link></li>)}
+      </ul>
 
-  listPoems(poems) {
-    return poems.map((poem) => <Poem key={poem.id} poem={poem} />)
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Poems</h1>
-        <ul>
-          {this.poems.map((poem) => <li key={poem.id}><Link to={this.match.url + '/' + poem.title.toLowerCase().split(" ").join("_")}>{poem.title}</Link></li>)}
-        </ul>
-
-        {this.poems.map((poem) => {
-          let path = this.match.url + '/' + poem.title.toLowerCase().split(" ").join("_")
-          return (
-            <Route key={poem.id} path={path} render={() => <Poem poem={poem} />} />
-          )
-        })}
-      </div>
-    )
-  }
+      {poems.map((poem) => {
+        let path = match.url + '/' + poem.title.toLowerCase().split(" ").join("_")
+        return (
+          <Route key={poem.id} path={path} render={() => <Poem poem={poem} />} />
+        )
+      })}
+    </div>
+  )
 }
 
 export default Poems
